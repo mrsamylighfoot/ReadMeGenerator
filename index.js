5e8660e747fb9d8eac0eb30c953ad7190f2901b3
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-04-15 18:15:06 -0400

    updated question types and included choice as appropriate, added init function

--- index.js
+++ index.js
@@ -2,63 +2,88 @@
 const licenseBadger = require('license-badger');
 const fs = require('fs');
 const inquirer = require('inquirer');
-const markdown = require
+const markdown = require('./utils/generateMarkdown');
+const util = require('util.promisify');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
+        type: 'input',
         message: 'What is your Project Title?',
-        name: 'title'
+        name: 'title',
     },
     {
+        type: 'input',
         message: 'Description: Provide a short description of your project.',
-        name: 'description'
+        name: 'description',
     },
     {
+       type: 'input',
         message: 'Installation: What do people need to know about installation?', 
-        name: 'installation'
+        name: 'installation',
     },
     {
+        type: 'input',
         message: 'Usage: Provice instructions for usage alonge with any examples.',
-        name: 'usage'
+        name: 'usage',
     },
     {
+        type: 'input',
         message: 'Collaborators: Who did you collaborate with? Did you follow any tutorials? Provide links here.',
-        name: 'collaborators'
+        name: 'collaborators',
     },
     {
+        type: 'list',
         message: 'License: Do you want to include an MIT License?',
-        name: 'license'
+        name: 'license',
+        choices: ['MIT', 'ISC', 'none'],
     },
     {
+        type: 'list',
         message: '"Do you want to include badges?',
-        name: 'badges'
+        name: 'badges',
+        choices: ['yes', 'no'],
     },
     {
+        type: 'input'
         message: 'Features: If your project has a lot of features, list them here.',
-        name: 'features'
+        name: 'features',
     },
     {
+        type: 'input',
         message: 'How to Contribute: How would you like other developers to contribute?',
-        name: 'contribute'
+        name: 'contribute',
     },
     {
+        type: 'input',
         message: 'Tests: Go the Extra Mile - write tests for your application and include here.',
-        name: 'tests'
+        name: 'tests',
     },
     {
+        type: 'input',
         message: 'What is your GitHub username?',
         name: 'github'
     }, 
     {
+        type: 'input',
         message: 'What is your email address?',
         name: 'email'
     },
-
+]
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((res) => {
+        console.log(res);
+        fs.writeFile('README.md', JSON.stringify(res), data),
+        console.log('Generating ReadMe');
+    })
+}
 
 // Function call to initialize app
 init();
