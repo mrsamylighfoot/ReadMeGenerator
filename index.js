ff647435a431b73a921154441e0d49a147afeafb
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-04-15 07:48:13 -0400

    listed required packages and framed out questions for prompts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
-const licenseBadger = require('license-badger')
+const licenseBadger = require('license-badger');
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your Project Title?", "Description: Provide a short description of your project.", "Installation: What do people need to know about installation?", "Usage: Provice instructions for usage alonge with any examples.", "Collaborators: Who did you collaborate with? Did you follow any tutorials? Provide links here.", "License: Do you want to include an MIT License?", "Do you want to include badges?", "Features: If your project has a lot of features, list them here.", "How to Contribute: How would you like other developers to contribute?", "Tests: Go the Extra Mile - write tests for your application and include here.", "What is your GitHub username?", "What is your email address?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
