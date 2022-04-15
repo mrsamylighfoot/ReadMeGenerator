27af8048c72da223c128ba8c8113bd7bfe54e8c0
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-04-15 06:57:01 -0400

    rearranged files for better flow and understanding

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-
+const licenseBadger = require('license-badger')
 // TODO: Create an array of questions for user input
 const questions = [];
 
